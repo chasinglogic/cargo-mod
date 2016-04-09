1c40fca694eafceccde8b53e85091a817f425b23
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-04-09 19:36:25 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+extern crate getopts;
+
+use getopts::Options;
+
+use std::path::Path;
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::env;
+
+fn print_usage() {
+    println!("Work in progress.")
+}
+
+fn open_file(p: &str) -> File {
+    let path = Path::new(p);
+    match File::create(&path) {
+        Err(e) => panic!("Unable to open file: {}", e),
+        Ok(file) => file,
+    }
+}
+
+fn write_file(f: &File, b: Bytes) {
+    match f.write_all(b) {
+        Err(e) => panic!("Unable to write to file: {}", e),
+        Ok(_) => println!("Generated module files."),
+    }
+}
+
+fn gen_folder_module(name: String, private: bool) {
+    let path_string = if cfg!(target_os = "windows") {
+        format!("{}\\mod.rs", name)
+    } else {
+        format!("{}/mod.rs", name)
+    };
+
+    let mut file = open_file(&path_string);
+    write_file(&file, format!("pub mod {}", name).as_bytes())
+}
+
+fn gen_module(name: String, private: bool) {
+    let path_string = format!("{}.rs", name);
+    let mut file = open_file(&path_string);
+
+}
+
+
+fn main() {
+    let mut opts = Options::new();
+    let args: Vec<String> = env::args().collect();
+
+    opts.optflag("p", "private", "Make the generated module private.");
+    opts.optflag("f", "folder", "Generate a folder module instead of a file.");
+
+    let matches = match opts.parse(&args) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+
+    let private = matches.opt_present("p");
+    let folder = matches.opt_present("f");
+    let name = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage();
+        return
+    };
+
+    if folder {
+        gen_folder_module(name, private);
+        return 
+    }
+
+    gen_module(name, private)
+}
