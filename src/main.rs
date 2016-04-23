8718e1bc2db119aba09d8423888e1b23388674d7
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-04-23 01:39:29 -0400

    Moved all module code to seperate module.

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,12 @@
 mod utils;
 
 extern crate getopts;
-
 use getopts::Options;
 
 use utils::*;
 
-use std::path::{Path, PathBuf};
-use std::io::prelude::*;
-use std::io;
-use std::fs;
-use std::env;
-
 fn print_usage() {
     println!("Work in progress.")
-}
-
-fn gen_folder_module(name: String, private: bool) {
-    let root_path = project::find_project_root(); // PathBuf
-    let mut our_path = root_path.clone();
-    our_path.push("src");
-    our_path.push(&name);
-
-    let res = fs::create_dir(our_path.as_path())
-        .expect("Unable to create directory");
-
-    println!("Created directory: {}", 
-             path::pretty_path(&root_path, &our_path).display());
-
-    our_path.push("mod.rs");
-    let mut f = fs::OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .append(true)
-        .open(our_path.as_path())
-        .expect("Unable to create mod.rs");
-
-    let result = f.write_all("".as_bytes())
-        .expect("Unable to create mod file");
-
-    println!("Generated mod file: {}", 
-             path::pretty_path(&root_path, &our_path).display()); 
-
-    module::add_mod(&root_path, module::generate_modstring(name, private)) 
-}
-
-fn gen_module(name: String, private: bool) {
-    let root_path = project::find_project_root();
-    let our_path = root.clone();
-    our_path.push("src");
-
-    // Then we have a folder module which we want to add to.
-    // if name.contains("/") {
-    //     let iter = name.split("/");
-    // }
-    
-    let mut namers = name.clone();
-    let dotrs: String = ".rs".to_string();
-    namers.push(dotrs);
-    our_path.push(namers);
-
-    let mut f = fs::File::create(our_path.as_path())
-        .expect("Unable to create mod file.");
-
-    f.write_all("".as_bytes());
-    println!("Created empty file: {}",
-             pretty_path(&root_path, &our_path).display());
-
-    module::add_mod(&root_path, module::generate_modstring(name, private))
 }
 
 fn main() {
@@ -76,7 +14,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     opts.optflag("p", "private", "Make the generated module private.");
-    opts.optflag("f", "folder", "Generate a folder module instead of a file.");
     opts.optflag("h", "help", "Show help message");
 
     let matches = match opts.parse(&args) {
@@ -85,7 +22,7 @@ fn main() {
     };
 
     if matches.opt_present("h") {
-        print_usage()
+        return print_usage()
     }
 
     let private = matches.opt_present("p");
@@ -96,10 +33,5 @@ fn main() {
         return
     };
 
-    if matches.opt_present("f") {
-        gen_folder_module(name, private);
-        return 
-    }
-
-    gen_module(name, private)
+    module::gen_module(name, private)
 }
