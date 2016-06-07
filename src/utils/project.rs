f168e07a3e2aa624d920d11c6ce8ea72e45fbf02
Mathew Robinson <mathew.robinson@kroger.com>
2016-06-07 15:12:37 -0400

    removed functions no longer required after refactor

--- src/utils/project.rs
+++ src/utils/project.rs
@@ -37,13 +37,6 @@ pub fn kind_of_crate(target_path: &PathBuf) -> CrateType {
     ModFile::Mod
 }
 
-pub fn find_project_root() -> PathBuf {
-    // Leaving this unwrap, we want to fail if for some reason we can't find current directory
-    let generated_path = gen_path_recurse(env::current_dir().unwrap().as_path());
-    println!("Generated: {}", generated_path.display());
-    generated_path
-}
-
 fn gen_path_recurse(cur_dir: &Path) -> PathBuf {
     match where_are_we(cur_dir) {
         Where::ProjectRoot => cur_dir.to_path_buf(),
@@ -56,19 +49,3 @@ fn gen_path_recurse(cur_dir: &Path) -> PathBuf {
         },
     }
 }
-
-fn where_are_we(cur_dir: &Path) -> Where {
-    // If Cargo.toml is here then we are at project root
-    let mut check = cur_dir.to_path_buf().clone();
-    check.push("Cargo.toml");
-
-    if fs::metadata(check).is_ok() {
-        return Where::ProjectRoot
-    }
-
-    Where::Other
-}
-
-
-
-
