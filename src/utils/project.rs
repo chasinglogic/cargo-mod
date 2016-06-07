f5079cff67b34b6888ba2f57f64be624f43e198e
Mathew Robinson <mathew.robinson@kroger.com>
2016-06-07 16:19:42 -0400

    removed dead code

--- src/utils/project.rs
+++ src/utils/project.rs
@@ -2,40 +2,7 @@ use std::path::{Path, PathBuf};
 use std::env;
 use std::fs;
 
-pub enum Where {
-    ProjectRoot,
-    Other,
-}
 
-pub enum ModFile {
-    Library,
-    Binary,
-    Mod,
-    Both,
-}
-
-pub fn kind_of_crate(target_path: &PathBuf) -> CrateType {
-    let mut lib_path = target_path.clone();
-    lib_path.push("lib.rs");
-
-    let mut bin_path = target_path.clone();
-    bin_path.push("main.rs");
-
-    if fs::metadata(lib_path.as_path()).is_ok() && 
-       fs::metadata(bin_path.as_path()).is_ok() {
-        return ModFile::Both
-    }
-
-    if fs::metadata(lib_path.as_path()).is_ok() {
-        return ModFile::Library
-    }
-
-    if fs::metadata(bin_path.as_path()).is_ok() {
-        return ModFile::Binary
-    }
-
-    ModFile::Mod
-}
 
 fn gen_path_recurse(cur_dir: &Path) -> PathBuf {
     match where_are_we(cur_dir) {
