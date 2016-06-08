92ad29a930f88979c36b32dd3c029a67508531aa
Mathew Robinson <mrobinson@localhost.localdomain>
2016-06-07 20:36:56 -0400

    removed dead code

--- src/utils/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod project;
-pub mod module;
-
-use std::path::PathBuf;
-
-pub fn pretty_path(root: &PathBuf, target: &PathBuf) -> PathBuf {
-    target.strip_prefix(root).unwrap().to_path_buf()
-}
